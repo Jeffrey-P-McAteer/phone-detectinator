b8bfd48c21e34e1b992c3412c13bf366c3e5f76f
Jeffrey McAteer <jeffrey.p.mcateer@gmail.com>
2021-08-21 12:39:28 -0400

    script beginnings

---
+++ phone_detectinator.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+
+# Script has 2 roles: to scan for
+# new devices under /var/lib/misc/dnsmasq.leases
+# and to serve a captive portal page on :80.
+# The captive portal lets people who bring phones
+# nearby turn off the alarm for their device.
+
+# MUST have permission to bind to :80
+
+import os
+import sys
+import subprocess
+import time
+import traceback
+import threading
+
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+# holds ['ab:cd:ef:12:34:45']
+macs_triggering_alarm = []
+
+# Holds [(expire_timestamp, 'ab:cd:ef:12:34:45')]
+ignored_macs = []
+
+class CaptivePortalHandler(BaseHTTPRequestHandler):
+  def do_GET(self):
+    global macs_triggering_alarm
+    global ignored_macs
+
+    # TODO allow a GET to /disengage-alarm to remove MAC
+    # from macs_triggering_alarm, and add an entry that
+    # expires in 5 minutes to ignored_macs
+
+
+    self.send_response(200)
+    self.end_headers()
+
+    self.wfile.write(bytes("Please turn off the alarm by clicking the following button:", "utf-8"))
+
+
+
+def run_http_server():
+  httpd = HTTPServer(('0.0.0.0', 80), CaptivePortalHandler)
+  httpd.serve_forever()
+
+def do_detection_poll():
+  global macs_triggering_alarm
+  global ignored_macs
+  with open('/var/lib/misc/dnsmasq.leases', 'r') as fd:
+    contents = fd.read()
+    for line in contents.splitlines():
+      line = line.strip()
+      if not line or len(line) < 3:
+        continue
+
+      mac = 'todo'
+      ignore_this_mac = False
+      for _, ignored_mac in ignored_macs:
+        if mac == ignored_mac:
+          ignore_this_mac = True
+          break
+      
+      if ignore_this_mac:
+        continue
+
+      if not mac in macs_triggering_alarm:
+        macs_triggering_alarm.append(mac)
+
+
+  # Sound alarm if len(macs_triggering_alarm) > 0:
+  if len(macs_triggering_alarm) > 0:
+    # This will run for ~8 seconds
+    subprocess.run([
+      'mpv', '/opt/alarm.mp3'
+    ], check=False)
+
+
+
+def main():
+  http_server_t = threading.Thread(target=run_http_server, args=())
+  http_server_t.start()
+  while True:
+    try:
+      do_detection_poll()
+    except:
+      traceback.print_exc()
+    
+    time.sleep(3)
+
+if __name__ == '__main__':
+  main()
+
--- readme.md
+++ readme.md
@@ -34,14 +34,30 @@ pacman -S sudo vim python
 
 ```
 
+3. (as root) Set a static IP on the wireless device (`wlan0` in this case):
+
+```bash
+cat > /etc/systemd/network/20-wired.network <<EOF
+[Match]
+Name=wlan0
+
+[Network]
+Address=10.0.0.1/16
+#Gateway=0.0.0.0
+#DNS=0.0.0.0
+EOF
+
+systemctl restart systemd-networkd
+
+```
+
 3. (as root) Install `hostapd`: (https://wiki.archlinux.org/title/software_access_point)
 
 ```bash
 pacman -S hostapd
 
 cat > /etc/hostapd/hostapd.conf <<EOF
-interface=wlan0_ap
-bridge=br0
+interface=wlan0
 
 # SSID to be used in IEEE 802.11 management frames
 ssid=JWAC_Phone_Detectinator
@@ -73,7 +89,7 @@ sudo systemctl enable --now hostapd.service
 sudo pacman -S pulseaudio
 
 # https://wiki.archlinux.org/title/PulseAudio#Starting_system-wide_on_boot
-cat > /etc/systemd/system <<EOF
+cat > /etc/systemd/system/pulseaudio.service <<EOF
 [Unit]
 Description=Sound Service
 
@@ -94,8 +110,59 @@ systemctl --global mask pulseaudio.socket
 ```
 
 
+4. Install `dnsmasq`:
+
+```bash
+pacman -S dnsmasq
+
+# edit /etc/systemd/resolved.conf and change DNSStubListener=no
+vim /etc/systemd/resolved.conf
+
+cat > /etc/dnsmasq.conf <<EOF
+interface=wlan0
+dhcp-range=10.0.0.10,10.0.10.250,5m
+# Set default gateway
+dhcp-option=3,10.0.0.1
+# Set DNS servers to announce
+dhcp-option=6,10.0.0.1
+log-queries
+except-interface=lo
+# Resolve every FQDN to ourselves
+address=/#/10.0.0.1
+EOF
+
+# By default Arch uses systemd-resolved on :53 for DNS caching
+
+systemctl enable --now dnsmasq.service
+
+# Leases issued get stored in /var/lib/misc/dnsmasq.leases
+
+```
+
+5. Copy over script and service files:
+
+`phone_detectinator.py` goes in `/opt/phone_detectinator.py`
+
+`alarm.mp3` goes in `/opt/alarm.mp3`
+
+`phone_detectinator.service` goes in `/etc/systemd/system/phone_detectinator.service`
+
+
+```bash
+# Start the code at boot and right now
+systemctl enable --now phone_detectinator.service
+
+```
 
 
 
+# Getting regular LAN internet back
+
+```bash
+sudo systemctl stop dnsmasq.service ; sudo systemctl start systemd-resolved
+# To reverse w/o a reboot
+sudo systemctl stop systemd-resolved ; sudo systemctl start dnsmasq.service
+
+```
 
 
